84cc34aace51bf8387f94c8b4439dee99fbf3730
Artem Koryakin <koryakin1993@inbox.ru>
2019-06-30 22:37:35 +0300

    hometask_2 start

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,20 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        //TOdo сделать так чтобы возвращало ожидаемые значения
+        val parts: List<String>? = fullName?.split(" ")
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO()
+    }
+}
